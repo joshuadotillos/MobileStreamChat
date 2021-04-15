67b97756b5da04fcd05d85d04908267b86bc4395
Joshua Andrei Nicolai Dotillos <joshua.dotillos.20@usjr.edu.ph>
2021-04-15 17:33:28 +0800

    fixed firebase

--- mobilestreamchat/lib/main.dart
+++ mobilestreamchat/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
-import 'package:crypto_wallet/ui/authentication.dart';
+import 'package:mobilestreamchat/ui/authentication.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
