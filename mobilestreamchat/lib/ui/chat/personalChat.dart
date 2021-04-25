5fa1c908455aeb572421193fcd2bacfcc6761048
Joshua Andrei Nicolai Dotillos <joshua.dotillos.20@usjr.edu.ph>
2021-04-25 21:38:04 +0800

    updated personal chat
    Personal Chat and Chat Message screen

--- mobilestreamchat/lib/ui/chat/personalChat.dart
+++ mobilestreamchat/lib/ui/chat/personalChat.dart
@@ -1,8 +1,9 @@
 import 'package:flappy_search_bar/search_bar_style.dart';
-import 'package:flutter/material.dart';
 import 'package:flappy_search_bar/flappy_search_bar.dart';
-import 'package:mobilestreamchat/model/messageDummy.dart';
 import 'package:mobilestreamchat/model/search.dart';
+
+import 'package:flutter/material.dart';
+import 'package:mobilestreamchat/ui/chat/recent_chats.dart';
 
 class PersonalChat extends StatefulWidget {
   @override
@@ -10,48 +11,58 @@ class PersonalChat extends StatefulWidget {
 }
 
 class _PersonalChatState extends State<PersonalChat> {
-  Future<List<Post>> search(String search) async {
-    await Future.delayed(Duration(seconds: 2));
-    if (search == "empty") return [];
-    return List.generate(search.length, (int index) {
-      return Post(
-        "Title : $search $index",
-        "Description :$search $index",
-      );
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Material(
-        child: Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 15.0),
-      child: SearchBar<Post>(
-        onSearch: search,
-        onItemFound: (Post post, int index) {
-          return ListTile(
-            title: Text(post.title),
-            subtitle: Text(post.description),
-          );
-        },
-        emptyWidget: Center(
-          child: Text("Empty"),
-        ),
-        hintText: "Search Name",
-        textStyle: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-            fontFamily: "Raleway"),
-        searchBarStyle: SearchBarStyle(
-          padding:
-              EdgeInsets.only(left: 10.0, right: 10.0, top: 5.0, bottom: 5.0),
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        header: Column(
-          children: [
-          ],
-        ),
+      child: Column(
+        children: [
+          RecentChats(),
+        ],
       ),
-    ));
+    );
   }
 }
+
+
+//For SearchBar
+// Future<List<Post>> search(String search) async {
+//     await Future.delayed(Duration(seconds: 2));
+//     if (search == "empty") return [];
+//     return List.generate(search.length, (int index) {
+//       return Post(
+//         "Title : $search $index",
+//         "Description :$search $index",
+//       );
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Material(
+//       child: Padding(
+//         padding: const EdgeInsets.symmetric(horizontal: 15.0),
+//         child: SearchBar<Post>(
+//           onSearch: search,
+//           onItemFound: (Post post, int index) {
+//             return ListTile(
+//               title: Text(post.title),
+//               subtitle: Text(post.description),
+//             );
+//           },
+//           emptyWidget: Center(
+//             child: Text("Empty"),
+//           ),
+//           hintText: "Search Name",
+//           textStyle: TextStyle(
+//               color: Colors.black,
+//               fontWeight: FontWeight.bold,
+//               fontFamily: "Raleway"),
+//           searchBarStyle: SearchBarStyle(
+//             padding:
+//                 EdgeInsets.only(left: 10.0, right: 10.0, top: 5.0, bottom: 5.0),
+//             borderRadius: BorderRadius.circular(10.0),
+//           ),
+//           header: Text("data"),
+//         ),
+//       ),
+//     );
