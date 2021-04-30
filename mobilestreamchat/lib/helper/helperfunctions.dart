0ae315d151eda68656c995110aa5e3ac3a041eca
Rhyan La Rosa <rhyan.larosa.20@usjr.edu.ph>
2021-04-30 13:06:59 +0800

    Conflict Resolved

--- mobilestreamchat/lib/helper/helperfunctions.dart
+++ mobilestreamchat/lib/helper/helperfunctions.dart
@@ -5,19 +5,38 @@ class HelperFuntions {
   static String sharedPreferenceUserNameKey = "USERNAMEKEY";
   static String sharedPreferenceUserEmailKey = "USEREMAILKEY";
 
-  static Future<void> saveUserLoggedInSharedPreference(
+  //SAVE DATA TO SHARED PREFS
+
+  static Future<bool> saveUserLoggedInSharedPreference(
       bool isUserLoggedIn) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     return await prefs.setBool(sharedPreferenceUserLoggedInKey, isUserLoggedIn);
   }
 
-  static Future<void> saveUserNameSharedPreference(String userName) async {
+  static Future<bool> saveUserNameSharedPreference(String userName) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     return await prefs.setString(sharedPreferenceUserNameKey, userName);
   }
 
-  static Future<void> saveUserEmailSharedPreference(String userEmail) async {
+  static Future<bool> saveUserEmailSharedPreference(String userEmail) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     return await prefs.setString(sharedPreferenceUserEmailKey, userEmail);
+  }
+
+  //GET DATA TO SHARED PREFS
+
+  static Future<bool> getUserLoggedInSharedPreference() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return await prefs.getBool(sharedPreferenceUserLoggedInKey);
+  }
+
+  static Future<String> getUserNameSharedPreference() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return await prefs.getString(sharedPreferenceUserNameKey);
+  }
+
+  static Future<String> getUserEmailSharedPreference() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return await prefs.getString(sharedPreferenceUserEmailKey);
   }
 }
